1db95fb2a169d0e3cd1fa95c62e1b50c283ecfef
lepoetemaudit <mail@davidjeffrey.co.uk>
2015-05-11 23:58:54 +0100

    Added genitive ordinals for "on" dates

--- polish/numbers_test.go
+++ polish/numbers_test.go
@@ -13,22 +13,30 @@ func TestGetCaseName(t *testing.T) {
 func TestGetPolishDate(t *testing.T) {
 
 	type DayMonth struct {
-		day   int
-		month int
+		day        int
+		month      int
+		standAlone bool
 	}
 
 	var dates = map[DayMonth]string{
-		DayMonth{1, 5}:   "pierwszy maja",
-		DayMonth{27, 1}:  "dwudziesty siódmy stycznia",
-		DayMonth{15, 12}: "piętnasty grudnia",
+		// Standalone dates
+		DayMonth{1, 5, true}:   "pierwszy maja",
+		DayMonth{27, 1, true}:  "dwudziesty siódmy stycznia",
+		DayMonth{15, 12, true}: "piętnasty grudnia",
+
+		// Non-standalone dates
+		DayMonth{13, 7, false}:  "trzynastego lipca",
+		DayMonth{31, 10, false}: "trzydziestego pierwszego października",
+		DayMonth{14, 2, false}:  "czternastego lutego",
 	}
 
 	for date, expected := range dates {
 		day := date.day
 		month := date.month
-		str, _ := GetPolishDate(day, month, true)
+		standAlone := date.standAlone
+		str, _ := GetPolishDate(day, month, standAlone)
 		if str != expected {
-			t.Fatalf(str)
+			t.Fatalf("Expected '%s' but got: '%s'", expected, str)
 		}
 	}
 
